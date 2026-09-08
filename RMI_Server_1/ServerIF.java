824fe2afe90fe31a3259f3cc83f863a3270ee9c8
junseokim <dev.barcat@gmail.com>
2026-09-08 21:07:02 +0900

    Modify : add grpc dir

--- RMI_Server_1/ServerIF.java
+++ RMI_Server_1/ServerIF.java
@@ -2,7 +2,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ServerIF extends Remote {
-	int add(int a, int b) throws RemoteException;
-	void addWd(String n) throws RemoteException;
-	String getWd(String name) throws RemoteException;
+	void addWd(String word) throws RemoteException;
+	String getWd(String clientName) throws RemoteException;
 }
